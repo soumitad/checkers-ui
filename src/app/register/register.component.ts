30be5084b84f135d8646fe4a598d4323847c29f9
Soumita Das <sd655@njit.edu>
2019-12-09 17:06:12 -0500

    Added proper validation for Registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 import { UserService, AuthenticationService } from '../_services';
@@ -23,24 +23,57 @@ export class RegisterComponent implements OnInit {
         private router: Router,
         private authenticationService: AuthenticationService,
         private userService: UserService
-    ) {
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) {
-            this.router.navigate(['/']);
-        }
-    }
-
+    ) {}
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
-            username: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(6)]]
-        });
+            username: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.compose([
+                // 1. Password Required
+                Validators.required,
+                // 2. Password should have one number
+                this._patternValidator(/\d/, { hasNumber: true }),
+                // 3. Password should have one uppercase
+                this._patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+                // 4. Password should have lowercase
+                this._patternValidator(/[a-z]/, { hasSmallCase: true }),
+                // 5. Min length of password should be 8 characters
+                Validators.minLength(8)
+            ])],
+            confirmPassword: ['', Validators.compose([
+                Validators.required
+            ])]
+        }, {
+                // check whether our password and confirm password match
+                validator: this._passwordMatchValidator
+            });
     }
 
     // convenience getter for easy access to form fields
     get f() { return this.registerForm.controls; }
+
+    private _patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } => {
+            if (!control.value) {
+                // if control is empty return no error
+                return null;
+            }
+            // test the value of the control against the regexp supplied
+            const valid = regex.test(control.value);
+            // if true, return no error (no error), else return error passed in the second parameter
+            return valid ? null : error;
+        };
+    }
+    private  _passwordMatchValidator(control: AbstractControl) {
+        const password: string = control.get('password').value; // get password from our password form control
+        const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
+        // compare is the password math
+        if (password !== confirmPassword) {
+            // if they don't match, set an error in our confirmPassword form control
+            control.get('confirmPassword').setErrors({ NoPasswordMatch: true });
+        }
+    }
 
     onSubmit() {
         this.submitted = true;
